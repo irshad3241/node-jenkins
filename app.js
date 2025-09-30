77f9eb141d207f7f5a48414a34348d8b0bddc9fd
Irshad <irshadshaikh5689133@gmail.com>
2025-09-30 18:25:35 +0530

    Jenkins added

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+const http = require('http');
+
+// Create a server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Heyy, Irshad shaikh!\n');
+});
+
+// Listen on port 3000
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
